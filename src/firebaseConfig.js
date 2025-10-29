665c7f415d8ebf671b39da286efa998cfb4a9f77
arrojasboris-byte <ar.rojasboris@gmail.com>
2025-10-29 01:46:26 +0100

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_BUCKET",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID",
+};
